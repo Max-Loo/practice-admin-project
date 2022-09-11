d2b42a4dd4691b6156ceb86991cf796799e9ab18
Max-Loo <luyuexing.scut@qq.com>
2022-09-11 17:57:39 +0800

    feat(layout): 主内容撑满屏幕

--- components/layouts/Index.vue
+++ components/layouts/Index.vue
@@ -3,10 +3,12 @@
     <LayoutSider />
     <a-layout :style="{ marginLeft: (store.collapsed ? 80 : 200 ) + 'px' }">
       <LayoutHeader />
-      <a-layout-content class="p-6 bg-gray-100">
+      <a-layout-content class="p-6 bg-gray-100 min-content-height">
         <slot />
       </a-layout-content>
-      <!-- <a-layout-footer class="text-center">footer</a-layout-footer> -->
+      <!-- <a-layout-footer class="text-center">
+        footer
+      </a-layout-footer> -->
     </a-layout>
   </a-layout>
 </template>
@@ -19,3 +21,8 @@ import { useLayoutCollapsedStore } from '~~/stores/useLayoutCollapsedStore'
 const store = useLayoutCollapsedStore()
 </script>
 
+<style scoped>
+.min-content-height {
+  min-height: calc(100vh - 64px);
+}
+</style>
